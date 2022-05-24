8fd850383651f576387e28ae2bab1554a5d0fc1a
Smashy <rocksubash2003@gmail.com>
2022-05-24 21:10:52 +0530

    json update

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req,res)=>{
+      const readstream = fs.createReadStream('./index.html');
+      res.writeHead(200,{'content-type': 'text/html'});
+      readstream.pipe(res);
+ }).listen(8080);
+ 
