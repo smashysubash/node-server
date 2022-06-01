2667980428ce001f46c9cb057ef73fb99c351a2e
Smashy <rocksubash2003@gmail.com>
2022-06-01 10:41:59 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const http = require('http');
-const fs = require('fs');
+var http=require('http');
+var fs=require('fs');
+
 http.createServer((req,res)=>{
-      const readstream = fs.createReadStream('./index.html');
-      res.writeHead(200,{'content-type': 'text/html'});
-      readstream.pipe(res);
- }).listen(8080);
- 
+    if(req.url==='/'){
+        fs.readFile('index.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url==='/about'){
+        fs.readFile('about.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url==='/contact-me'){
+        fs.readFile('contact-me.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else{
+        fs.readFile('404.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+}).listen(8080);
